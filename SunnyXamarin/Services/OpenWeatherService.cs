fdbbddcd82b191737de3e38f0bba91c21907907f
TomasPavlik <tomaes98sv@gmail.com>
2021-05-22 18:07:10 +0200

    Working with GeoLocation

---
+++ SunnyXamarin/Services/OpenWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunnyXamarin.Services
+{
+    public class OpenWeatherService
+    {
+        const string baseURL= "api.openweathermap.org/data/2.5/weather";
+        const string apiKey = "appid=f491b607f1c1e1bb4af977bb5581ba86";
+
+    }
+}
