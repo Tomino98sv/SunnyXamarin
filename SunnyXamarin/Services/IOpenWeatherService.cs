6e0c189d85fe4423ff777e2099beb8ada9d89598
TomasPavlik <tomaes98sv@gmail.com>
2021-05-22 23:09:09 +0200

    WeatherByLocation
    Main functionality done

---
+++ SunnyXamarin/Services/IOpenWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using SunnyXamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunnyXamarin.Services
+{
+    interface IOpenWeatherService
+    {
+        Task<WeatherModel> getWeatherByLocation(string longitude, string latitude);
+    }
+}
--- SunnyXamarin/Services/OpenWeatherService.cs
+++ SunnyXamarin/Services/OpenWeatherService.cs
@@ -1,13 +1,48 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SunnyXamarin.Models;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SunnyXamarin.Services
 {
-    public class OpenWeatherService
+    public class OpenWeatherService : IOpenWeatherService
     {
-        const string baseURL= "api.openweathermap.org/data/2.5/weather";
+        const string baseURL= "http://api.openweathermap.org/data/2.5/weather";
         const string apiKey = "appid=f491b607f1c1e1bb4af977bb5581ba86";
+
+        public async Task<WeatherModel> getWeatherByLocation(string longitude, string latitude)
+        {
+            try
+            {
+                var request = new HttpRequestMessage();
+                string url = baseURL + "?lat=" + latitude + "&lon=" + longitude + "&" + apiKey;
+                request.RequestUri = new Uri(url);
+                request.Method = HttpMethod.Get;
+                request.Headers.Add("Accept", "application/json");
+                var client = new HttpClient();
+                HttpResponseMessage response = await client.SendAsync(request);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    HttpContent content = response.Content;
+                    var json = await content.ReadAsStringAsync();
+                    WeatherModel weather = JsonConvert.DeserializeObject<WeatherModel>(json);
+
+                    return weather;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return null;
+
+
+
+        }
 
     }
 }
